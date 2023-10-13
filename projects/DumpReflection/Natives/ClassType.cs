3c08ad8c814d970a3c474a576ed4b567f5f032cf
Rick Gibbed <rick@gibbed.us>
2023-10-13 15:29:52 -0500

    Add DumpReflection project.

---
+++ projects/DumpReflection/Natives/ClassType.cs
@@ -0,0 +1,52 @@
+﻿/* Copyright (c) 2023 Rick (rick 'at' gibbed 'dot' us)
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace DumpReflection.Natives
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal class ClassType
+    {
+        public IntPtr Vftable;
+        public uint Size;
+        public ushort Alignment;
+        public byte Unknown0E;
+        public byte Unknown0F;
+        public IntPtr Next;
+        public IntPtr Name;
+        public IntPtr Unknown20;
+        public IntPtr Unknown28;
+        public IntPtr Unknown30;
+        public IntPtr Unknown38;
+        public StdVector Fields;
+        public IntPtr Unknown58;
+        public IntPtr Unknown60;
+        public IntPtr Unknown68;
+        public IntPtr Unknown70;
+        public IntPtr Unknown78;
+        public IntPtr Unknown80;
+        public uint Unknown88;
+        public ushort Unknown8C;
+    }
+}
---
+++ projects/DumpReflection/Program.cs
@@ -0,0 +1,307 @@
+﻿/* Copyright (c) 2023 Rick (rick 'at' gibbed 'dot' us)
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Gibbed.AddressLibrary;
+using StarfieldDumping;
+
+namespace DumpReflection
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Environment.ExitCode = RealMain(args);
+            if (System.Diagnostics.Debugger.IsAttached == false)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static int RealMain(string[] args)
+        {
+            var process = DumpingHelpers.FindSuitableProcess();
+            if (process == null)
+            {
+                Console.WriteLine("Failed to find suitable Starfield process.");
+                return -1;
+            }
+
+            var isSteamVersion = process.Modules
+                .Cast<System.Diagnostics.ProcessModule>()
+                .Any(m => IsSteamModule(m.ModuleName) == true);
+            var isMsStoreVersion = isSteamVersion == false;
+
+            AddressLibrary addressLibrary;
+            try
+            {
+                addressLibrary = AddressLibraryLoader.LoadFor(process.MainModule.FileName, isMsStoreVersion);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception when fetching address library for Starfield:");
+                Console.WriteLine(e);
+                return -2;
+            }
+
+            if (addressLibrary == null)
+            {
+                Console.WriteLine("Failed to load address library for Starfield.");
+                return -3;
+            }
+
+            using RuntimeProcess runtime = new();
+            if (runtime.OpenProcess(process) == false)
+            {
+                Console.WriteLine("Failed to open Starfield process.");
+                return -4;
+            }
+
+            Dump(runtime, addressLibrary);
+            return 0;
+        }
+
+        private static bool IsSteamModule(string name)
+        {
+            return name.StartsWith("steam_api64", StringComparison.OrdinalIgnoreCase) == true;
+        }
+
+        private static void Dump(RuntimeProcess runtime, AddressLibrary addressLibrary)
+        {
+            var mainModuleBaseAddressValue = runtime.Process.MainModule.BaseAddress.ToInt64();
+            IntPtr Id2Pointer(ulong id)
+            {
+                var offset = (long)addressLibrary[id];
+                return new(mainModuleBaseAddressValue + offset);
+            }
+
+            var classTypes = ReadClassTypes(runtime, Id2Pointer);
+            var enumTypes = ReadEnumTypes(runtime, Id2Pointer);
+
+            Dictionary<IntPtr, string> typeNames = new();
+            foreach (var (typeId, typeName) in GetTypeNames())
+            {
+                typeNames[Id2Pointer(typeId)] = typeName;
+            }
+
+            Dictionary<IntPtr, ClassType> classTypeMap = new();
+            foreach (var instance in classTypes)
+            {
+                classTypeMap.Add(instance.Pointer, instance);
+            }
+
+            Dictionary<IntPtr, EnumType> enumTypeMap = new();
+            foreach (var instance in enumTypes)
+            {
+                enumTypeMap.Add(instance.Pointer, instance);
+            }
+
+            foreach (var instance in classTypes)
+            {
+                Console.WriteLine($"{instance.Name}");
+                foreach (var field in instance.Fields)
+                {
+                    string typeName;
+                    if (classTypeMap.TryGetValue(field.Type, out var classType) == true)
+                    {
+                        typeName = classType.Name;
+                    }
+                    else if (enumTypeMap.TryGetValue(field.Type, out var enumType) == true)
+                    {
+                        typeName = enumType.Name;
+                    }
+                    else if (typeNames.TryGetValue(field.Type, out typeName) == false)
+                    {
+                        typeName = $"unknown:{runtime.ToStaticAddress(field.Type):X}";
+                    }
+
+                    Console.WriteLine($"  {field.Name} : {typeName} @{field.Offset:X}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static IEnumerable<(ulong id, string name)> GetTypeNames()
+        {
+            yield return (780674, "int16_t");
+            yield return (780678, "int8_t");
+            yield return (780680, "uint8_t");
+            yield return (780684, "int64_t");
+            yield return (780687, "double");
+            yield return (780690, "uint16_t");
+            yield return (780692, "int32_t");
+            yield return (780696, "bool");
+            yield return (780699, "float");
+            yield return (780701, "uint64_t");
+            yield return (780704, "uint32_t");
+
+            yield return (780711, "BSFixedString");
+
+            yield return (887377, "BSReflection::TESFormPointer<ActorValueInfo *>");
+            yield return (887380, "BSReflection::TESFormPointer<TESObjectLIGH *>");
+            yield return (887383, "BSReflection::TESFormPointer<BGSArtObject *>");
+
+            yield return (772740, "BSTArray<TESForm *>");
+            yield return (772741, "BSReflection::StdUniquePtrType<BSAttachConfig::IAttachableObject>");
+
+            yield return (780005, "BSTArray<std::unique_ptr<BSSequence::Event>>");
+        }
+
+        private static List<ClassType> ReadClassTypes(RuntimeProcess runtime, Func<ulong, IntPtr> id2Pointer)
+        {
+            var classTypesPointer = id2Pointer(885835);
+            var classTypeVftablePointer = id2Pointer(285167);
+
+            List<Natives.ClassType> natives = new();
+            List<ClassType> instances = new();
+            IntPtr next = runtime.ReadPointer(classTypesPointer);
+            while (next != IntPtr.Zero && next != classTypesPointer)
+            {
+                var current = next;
+                var native = runtime.ReadStructure<Natives.ClassType>(current);
+                next = native.Next;
+
+                if (native.Vftable != classTypeVftablePointer)
+                {
+                    var vftable = runtime.ToStaticAddress(native.Vftable);
+                    throw new InvalidOperationException($"unknown vftable @ {vftable:X}");
+                }
+
+                natives.Add(native);
+                ClassType instance = ReadClassType(runtime, current);
+                instances.Add(instance);
+            }
+            return instances;
+        }
+
+        private static ClassType ReadClassType(RuntimeProcess runtime, IntPtr nativePointer)
+        {
+            var native = runtime.ReadStructure<Natives.ClassType>(nativePointer);
+
+            ClassType instance = new();
+            instance.Pointer = nativePointer;
+            instance.Name = runtime.ReadStringZ(native.Name, Encoding.ASCII);
+            instance.Size = native.Size;
+            instance.Alignment = native.Alignment;
+
+            var fieldSize = Marshal.SizeOf(typeof(Natives.ClassField));
+
+            List<ClassField> fields = new();
+            for (var fieldPointer = native.Fields.Start; fieldPointer != native.Fields.End; fieldPointer += fieldSize)
+            {
+                var field = ReadClassField(runtime, fieldPointer);
+                fields.Add(field);
+            }
+
+            instance.Fields.AddRange(fields);
+
+            return instance;
+        }
+
+        private static ClassField ReadClassField(RuntimeProcess runtime, IntPtr nativePointer)
+        {
+            var native = runtime.ReadStructure<Natives.ClassField>(nativePointer);
+
+            if (native.Unknown14 != 0 ||
+                native.Unknown18 != IntPtr.Zero ||
+                native.Unknown20 != IntPtr.Zero ||
+                native.Unknown28 != IntPtr.Zero ||
+                native.Unknown30 != -1 ||
+                native.Unknown34 != -1)
+            {
+
+            }
+
+            ClassField instance = new();
+            instance.Name = runtime.ReadStringZ(native.Name, Encoding.ASCII);
+            instance.Type = native.Type;
+            instance.Offset = native.Offset;
+            return instance;
+        }
+
+        private static List<EnumType> ReadEnumTypes(RuntimeProcess runtime, Func<ulong, IntPtr> id2Pointer)
+        {
+            IntPtr enumTypesPointer = id2Pointer(885839);
+            IntPtr enumTypeVftablePointer = id2Pointer(292531);
+
+            List<Natives.EnumType> natives = new();
+            List<EnumType> instances = new();
+            IntPtr next = runtime.ReadPointer(enumTypesPointer);
+            while (next != IntPtr.Zero && next != enumTypesPointer)
+            {
+                var current = next;
+                var native = runtime.ReadStructure<Natives.EnumType>(current);
+                next = native.Next;
+
+                if (native.Vftable != enumTypeVftablePointer)
+                {
+                    var vftable = runtime.ToStaticAddress(native.Vftable);
+                    throw new InvalidOperationException($"unknown vftable @ {vftable:X}");
+                }
+
+                natives.Add(native);
+                EnumType instance = ReadEnumType(runtime, current);
+                instances.Add(instance);
+            }
+            return instances;
+        }
+
+        private static EnumType ReadEnumType(RuntimeProcess runtime, IntPtr nativePointer)
+        {
+            var native = runtime.ReadStructure<Natives.EnumType>(nativePointer);
+
+            EnumType instance = new();
+            instance.Pointer = nativePointer;
+            instance.Name = runtime.ReadStringZ(native.Name, Encoding.ASCII);
+            instance.Size = native.Size;
+            instance.Alignment = native.Alignment;
+            instance.Unknown0E = native.Unknown0E;
+
+            var memberSize = Marshal.SizeOf(typeof(Natives.EnumMember));
+
+            List<EnumMember> members = new();
+            for (var memberPointer = native.Members.Start; memberPointer != native.Members.End; memberPointer += memberSize)
+            {
+                var member = ReadEnumMember(runtime, memberPointer);
+                members.Add(member);
+            }
+
+            instance.Members.AddRange(members);
+
+            return instance;
+        }
+
+        private static EnumMember ReadEnumMember(RuntimeProcess runtime, IntPtr nativePointer)
+        {
+            var native = runtime.ReadStructure<Natives.EnumMember>(nativePointer);
+
+            EnumMember instance = new();
+            instance.Name = runtime.ReadStringZ(native.Name, Encoding.ASCII);
+            instance.Value = native.Value;
+            return instance;
+        }
+    }
+}
